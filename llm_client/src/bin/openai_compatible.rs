491fa52beb82a8488f8083c724cc25ac5cbb6b31
skcd <sandeepkumarpani888@gmail.com>
2024-11-28 20:53:58 +0000

    [sidecar] improve script for openai-compatible

--- llm_client/src/bin/openai_compatible.rs
+++ llm_client/src/bin/openai_compatible.rs
@@ -6,8 +6,12 @@ use llm_client::provider::OpenAICompatibleConfig;
 
 #[tokio::main]
 async fn main() {
-    let api_key = std::env::var("api_key").expect("to be present");
-    let api_base = std::env::var("api_base").expect("to work");
+    let api_key = "".to_owned();
+    // put your api base over here
+    let api_base = "".to_owned();
+    // put the model name over here, not sure if this is really required for LMStudio?
+    let model_name = "".to_owned();
+
     let openai_client = OpenAICompatibleClient::new();
     let api_key =
         llm_client::provider::LLMProviderAPIKeys::OpenAICompatible(OpenAICompatibleConfig {
@@ -15,7 +19,7 @@ async fn main() {
             api_base,
         });
     let request = LLMClientCompletionRequest::new(
-        llm_client::clients::types::LLMType::Custom("gpt-4o".to_owned()),
+        llm_client::clients::types::LLMType::Custom(model_name.to_owned()),
         vec![LLMClientMessage::system(
             "tell me how to add 2 numbers in rust".to_owned(),
         )],
@@ -26,5 +30,7 @@ async fn main() {
     let response = openai_client
         .stream_completion(api_key, request, sender)
         .await;
+
+    // wait for the magic to show up in your stdout
     dbg!(&response);
 }

6deffd0929f697a6fd66536ef21e0554d080bc3e
skcd <sandeepkumarpani888@gmail.com>
2024-12-06 20:41:11 +0000

    [sidecar] disable cd for the agent

--- sidecar/src/agentic/tool/session/tool_use_agent.rs
+++ sidecar/src/agentic/tool/session/tool_use_agent.rs
@@ -313,13 +313,14 @@ Can you help me implement the necessary changes to the repository {repo_name} so
 I've already taken care of all changes to any of the test files described in the <pr_description>. This means you DON'T have to modify the testing logic or any of the tests in any way!
 I've also setup the developer environment in {working_directory} for {repo_name}. This means you DON'T have to install any new libraries in any way!
 
-Your task is to make the minimal changes to non-tests files in the /repo directory to ensure the <pr_description> is satisfied.
+Your task is to make the minimal changes to non-tests files in the {working_directory} directory to ensure the <pr_description> is satisfied.
 
 Tool capabilities:
 - You have access to tools that let you execute CLI commands on the local checkout, list files, view source code definitions, regex search, read and write files. These tools help you effectively accomplish a wide range of tasks, such as writing code, making edits or improvements to existing files, understanding the current state of a project, and much more.
 - You can use search_files to perform regex searches across files in a specified directory, outputting context-rich results that include surrounding lines. This is particularly useful for understanding code patterns, finding specific implementations, or identifying areas that need refactoring.
 - When using the search_files tool, craft your regex patterns carefully to balance specificity and flexibility. Based on the Github Issue you may use it to find code patterns, function definitions, or any text-based information across the project. The results include context, so analyze the surrounding code to better understand the matches. Leverage the search_files tool in combination with other tools for more comprehensive analysis.
-- When using execute_command tool to run python scripts, only use the relative file path and never absolute. This means all your file paths in execute_command should be relative.
+- With execute_command you are not allowed to `cd` and are stuck in the {working_directory} all your commands should use paths relative to this and `cd` is NOT ALLOWED.
+- When using execute_command tool to run python scripts, only use the relative file path and never absolute. This means all your file paths in execute_command should be relative. If you want to execute reproduce_error.py, then use `python reproduce.py` no need to `cd` to the {working_directory}.
 
 ====
 
